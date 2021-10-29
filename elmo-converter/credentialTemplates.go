d9a76e0bcd02ed1dbb244dd9d0587dc1e3d01dec
Leon <lerosoft@live.de>
2021-10-29 02:40:58 +0200

    fixed plain xml readout of input. detected type of document. startet modular parsing approach.

---
+++ elmo-converter/credentialTemplates.go
@@ -0,0 +1,11 @@
+package main
+
+type Learner struct {
+	Citizenship string `xml:"citizenship"`
+}
+
+type EducationCredential interface {
+}
+
+type Abitur struct {
+}
--- elmo-converter/elmo-converter.go
+++ elmo-converter/elmo-converter.go
@@ -9,25 +9,35 @@ import (
 func main() {
 	log.Println("### elmo converter ###")
 
-	//check if path is given
-	if len(os.Args) < 1 {
+	/// --- PARSING XML --- ///
 
+	// check if path is given
+	if len(os.Args) < 1 {
+		log.Fatal("no path given")
 	}
+
 	// read xml path from command line argument
 	var xmlPath = os.Args[1]
 	f, err := os.Open(xmlPath)
-	log.Println("parsing xml file at relative path %s", xmlPath)
+	log.Printf("parsing xml file at relative path %s\n", xmlPath)
 	if err != nil {
 		f.Close()
 		log.Fatalf("failed to open xml at %s: %v", xmlPath, err)
 	}
-	doc, err := xmlquery.Parse(f)
+	parsed, err := xmlquery.Parse(f)
 	if err != nil {
 		f.Close()
 		log.Fatalf("failed to parse xml: %v", err)
-	} else {
-		log.Printf("parsing of %s successfull:\n%v", xmlPath, doc)
 	}
+	// create Navigator to move through xml structure
+	doc := xmlquery.CreateXPathNavigator(parsed)
+	log.Printf("parsing of %s successfull!", doc.Current().InnerText())
 	f.Close()
+
+	// --------------
+	// 		QUERY
+	// --------------
+	docType := xmlquery.FindOne(doc.Current(), "//learningOpportunitySpecification//title[@xml:lang='en']").InnerText()
+	log.Printf("type of document: %s\n", docType)
 
 }
