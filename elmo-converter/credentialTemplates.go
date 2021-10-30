50aad69655a536b908aea0b66055c5df4ab397d9
Leon <lerosoft@live.de>
2021-10-30 17:55:42 +0200

    first working xml -> json in console. Lot of details still missing

--- elmo-converter/credentialTemplates.go
+++ elmo-converter/credentialTemplates.go
@@ -1,11 +1,69 @@
 package main
 
-type Learner struct {
-	Citizenship string `xml:"citizenship"`
+import (
+	"encoding/json"
+	"encoding/xml"
+	"log"
+)
+
+type Identifier struct {
+	Type  string `xml:"type,attr"`
+	Value string `xml:",innerxml"`
 }
 
+type Title struct {
+	Lang  string `xml:"lang,attr"`
+	Value string `xml:",innerxml"`
+}
+
+type Issuer struct {
+	Country    string     `xml:"country"`
+	Identifier Identifier `xml:"identifier"`
+	Title      Title      `xml:"title"`
+	URL        string     `xml:"url"`
+}
+
+type Learner struct {
+	Citizenship  string       `xml:"citizenship"`
+	Identifiers  []Identifier `xml:"identifier"`
+	GivenNames   string       `xml:"givenNames"`
+	FamilyName   string       `xml:"familyName"`
+	Bday         string       `xml:"bday"`
+	PlaceOfBirth string       `xml:"placeOfBirth"`
+	Gender       int          `xml:"gender"`
+}
+
+type Report struct {
+	Issuer Issuer `xml:"issuer"`
+	// TODO add learning opportunities
+}
 type EducationCredential interface {
+	ToJson() string
 }
 
 type Abitur struct {
+	XMLName       xml.Name `xml:"elmo"`
+	GeneratedDate string   `xml:"generatedDate"`
+	Learner       Learner  `xml:"learner"`
+	Reports       []Report `xml:"report"`
+}
+
+func (a *Abitur) ToJson() string {
+	data, err := json.MarshalIndent(a, "", "  ")
+	if err != nil {
+		log.Fatalf("errpr while encoding to json")
+		return ""
+	}
+	return string(data)
+}
+
+func CreateCredential(docType string) EducationCredential {
+	switch docType {
+	case "Abitur":
+		return &Abitur{
+			GeneratedDate: "none",
+		}
+	default:
+		return nil
+	}
 }
--- elmo-converter/elmo-converter.go
+++ elmo-converter/elmo-converter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"github.com/antchfx/xmlquery"
 	"log"
 	"os"
@@ -17,7 +18,7 @@ func main() {
 	}
 
 	// read xml path from command line argument
-	var xmlPath = os.Args[1]
+	xmlPath := os.Args[1]
 	f, err := os.Open(xmlPath)
 	log.Printf("parsing xml file at relative path %s\n", xmlPath)
 	if err != nil {
@@ -27,17 +28,31 @@ func main() {
 	parsed, err := xmlquery.Parse(f)
 	if err != nil {
 		f.Close()
-		log.Fatalf("failed to parse xml: %v", err)
+		log.Fatalf("failed to read xml: %v", err)
 	}
+	f.Close()
 	// create Navigator to move through xml structure
 	doc := xmlquery.CreateXPathNavigator(parsed)
-	log.Printf("parsing of %s successfull!", doc.Current().InnerText())
-	f.Close()
+	// move to elmo xml node
+	doc.MoveToChild()
+	doc.MoveToNext()
+	// print current node data
+	log.Printf("reading of %s successfull!", doc.Current().InnerText())
 
 	// --------------
 	// 		QUERY
 	// --------------
 	docType := xmlquery.FindOne(doc.Current(), "//learningOpportunitySpecification//title[@xml:lang='en']").InnerText()
 	log.Printf("type of document: %s\n", docType)
+
+	// create struct to parse xml into
+	credential := CreateCredential(docType)
+	// dump xml data into struct
+	err = xml.Unmarshal([]byte(doc.Current().OutputXML(true)), &credential)
+	if err != nil {
+		log.Fatalf("error while parsing xml file: %s", err)
+	}
+	// log result
+	log.Printf("parsing successful: \n%s", credential.ToJson())
 
 }
