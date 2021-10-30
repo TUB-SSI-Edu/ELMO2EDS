d20922ef4691e366fa4e3bca8f098a79366712ce
Leon <lerosoft@live.de>
2021-10-30 18:55:49 +0200

    cleanup. moved functionality to standalone functions for better code clarity

--- elmo-converter/elmo-converter.go
+++ elmo-converter/elmo-converter.go
@@ -7,6 +7,42 @@ import (
 	"os"
 )
 
+func getTypeFromXML(navigator *xmlquery.NodeNavigator) string {
+	return xmlquery.FindOne(navigator.Current(), "//learningOpportunitySpecification//title[@xml:lang='en']").InnerText()
+}
+
+func loadXML(xmlPath string) *xmlquery.NodeNavigator {
+	// open file
+	f, err := os.Open(xmlPath)
+	log.Printf("parsing xml file at relative path %s\n", xmlPath)
+	if err != nil {
+		f.Close()
+		log.Fatalf("failed to open xml at %s: %v", xmlPath, err)
+	}
+	// read XML
+	parsed, err := xmlquery.Parse(f)
+	if err != nil {
+		f.Close()
+		log.Fatalf("failed to read xml: %v", err)
+	}
+	f.Close()
+	// create Navigator to move through xml structure
+	elmoNode := xmlquery.FindOne(parsed, "//elmo")
+	doc := xmlquery.CreateXPathNavigator(elmoNode)
+	return doc
+}
+
+func parseXMLToCredential(navigator *xmlquery.NodeNavigator) EducationCredential {
+	// create struct to parse xml into
+	credential := CreateCredential(getTypeFromXML(navigator))
+	// dump xml data into struct
+	err := xml.Unmarshal([]byte(navigator.Current().OutputXML(true)), &credential)
+	if err != nil {
+		log.Fatalf("error while parsing xml file: %s", err)
+	}
+	return credential
+}
+
 func main() {
 	log.Println("### elmo converter ###")
 
@@ -16,43 +52,26 @@ func main() {
 	if len(os.Args) < 1 {
 		log.Fatal("no path given")
 	}
-
 	// read xml path from command line argument
 	xmlPath := os.Args[1]
-	f, err := os.Open(xmlPath)
-	log.Printf("parsing xml file at relative path %s\n", xmlPath)
-	if err != nil {
-		f.Close()
-		log.Fatalf("failed to open xml at %s: %v", xmlPath, err)
-	}
-	parsed, err := xmlquery.Parse(f)
-	if err != nil {
-		f.Close()
-		log.Fatalf("failed to read xml: %v", err)
-	}
-	f.Close()
-	// create Navigator to move through xml structure
-	doc := xmlquery.CreateXPathNavigator(parsed)
-	// move to elmo xml node
-	doc.MoveToChild()
-	doc.MoveToNext()
+
+	doc := loadXML(xmlPath)
 	// print current node data
 	log.Printf("reading of %s successfull!", doc.Current().InnerText())
 
 	// --------------
 	// 		QUERY
 	// --------------
-	docType := xmlquery.FindOne(doc.Current(), "//learningOpportunitySpecification//title[@xml:lang='en']").InnerText()
+	docType := getTypeFromXML(doc)
 	log.Printf("type of document: %s\n", docType)
 
-	// create struct to parse xml into
-	credential := CreateCredential(docType)
-	// dump xml data into struct
-	err = xml.Unmarshal([]byte(doc.Current().OutputXML(true)), &credential)
-	if err != nil {
-		log.Fatalf("error while parsing xml file: %s", err)
-	}
+	// parse XML to struct
+	credential := parseXMLToCredential(doc)
+
+	// parse to JSON
+	json := credential.ToJson()
+
 	// log result
-	log.Printf("parsing successful: \n%s", credential.ToJson())
+	log.Printf("parsing successful: \n%s", json)
 
 }
