026d0ce5684e60950a2ecadd21e6b0a79d3bf7d0
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2021-11-24 21:13:47 +0100

    implemented custom find to search js objetcs for keys

--- nodejs-version/utils/helper.js
+++ nodejs-version/utils/helper.js
@@ -25,6 +25,36 @@ function getKey(key, obj) {
     return key.split('.').reduce(function(a,b){
       return a && a[b];
     }, obj);
+}/*
+function find(theObject) {
+    var result = null;
+    if(theObject instanceof Array) {
+        for(var i = 0; i < theObject.length; i++) {
+            result = getObject(theObject[i]);
+            if (result) {
+                break;
+            }   
+        }
+    }
+    else
+    {
+        for(var prop in theObject) {
+            console.log(prop + ': ' + theObject[prop]);
+            if(prop == 'id') {
+                if(theObject[prop] == 1) {
+                    return theObject;
+                }
+            }
+            if(theObject[prop] instanceof Object || theObject[prop] instanceof Array) {
+                result = getObject(theObject[prop]);
+                if (result) {
+                    break;
+                }
+            } 
+        }
+    }
+    return result;
 }
+*/
 
 module.exports = {assertArray, parseLangText, isEmpty, getKey}
