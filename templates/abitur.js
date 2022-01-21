507a7c21dd4dd8a3de91f3692bd59e399770e682
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2022-01-21 18:11:01 +0100

    implemented template inheritance

--- templates/abitur.js
+++ templates/abitur.js
@@ -1,32 +1,17 @@
 const utils = require('../utils/helper')
+const base  = require('./_baseTemplate')
 
 const keywords = ["abitur"]
 
-class Issuer {
+class Issuer extends base.IssuerStub{
     constructor(issuer, levels){
-            this.url = issuer.url;
-            this.country = issuer.country;
-            utils.multiTagParser("title", "xml:lang", issuer, this);
-            utils.multiTagParser("description", "xml:lang", issuer, this);
-            // issuer id
-            this. identifier = utils.parseIdentifier(issuer);
-            // levels
-            for (const level of utils.assertArray(levels)) {
-                this["level"+level.type.toUpperCase()] = level.value
-            }
+        super(issuer, levels)
     }
 }
 
-class CredentialSubject {
+class CredentialSubject extends base.CredentialSubjectStub{
     constructor(learner){
-        this.givenName = learner.givenNames
-        this.familyName = learner.familyName
-        this.fullName = this.givenName +" "+this.familyName
-        this.citizenship = learner.citizenship
-        this.bday = learner.bday
-        this.placeOfBirth = learner.placeOfBirth
-        this.gender = learner.gender
-        this.achieved = []
+        super(learner)
     }
 
     addDegree(learnerLOS, credits) {
